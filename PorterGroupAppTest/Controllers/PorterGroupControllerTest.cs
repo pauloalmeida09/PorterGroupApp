cb9b6e7ee53e5a9baff69c6ecdc64221d2f8362a
Paulo Victor Almeida <pvaalmeida.a@outlook.com>
2023-05-10 01:14:45 -0300

    respondendo as questões

---
+++ PorterGroupAppTest/Controllers/PorterGroupControllerTest.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PorterGroupApp.Controllers;
+using PorterGroupApp.Interfaces;
+using PorterGroupApp.Models.Request;
+using PorterGroupApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PorterGroupAppTest.Controllers
+{
+    public class PorterGroupControllerTest
+    {
+        public PorterGroupController StartupController(Mock<ILogger<PorterGroupController>> logger = null,
+                                     Mock<IConvertNumberRepository> convertNumberRepository = null,
+                                     Mock<ISumNumberRepository> sumNumberRepository = null,
+                                     Mock<IOperationMathResolveRepository> operationMathResolveRepository = null,
+                                     Mock<IListObjectUnicRepository> listObjectUnicRepository = null)
+        {
+            var mockLogger = logger == null ? new Mock<ILogger<PorterGroupController>>() : logger;
+            var mockConverterNumber = convertNumberRepository == null ? new Mock<IConvertNumberRepository>() : convertNumberRepository;
+            var mockSumNumbers = sumNumberRepository == null ? new Mock<ISumNumberRepository>() : sumNumberRepository;
+            var mockOperationMath = operationMathResolveRepository == null ? new Mock<IOperationMathResolveRepository>() : operationMathResolveRepository;
+            var mockListObject = listObjectUnicRepository == null ? new Mock<IListObjectUnicRepository>() : listObjectUnicRepository;
+
+            return new PorterGroupController(mockLogger.Object,
+                                             mockConverterNumber.Object,
+                                             mockSumNumbers.Object,
+                                             mockOperationMath.Object,
+                                             mockListObject.Object);
+
+        }
+
+        [Fact]
+        public void ControllerPorterGroup_NumbersToWord_Succsess()
+        {
+            try
+            {
+                int numberToWord = 12;
+                string wordNumber = "doze";
+
+                var objectReturn = new NumbersToWordRequest(numberToWord);
+                
+                Mock<IConvertNumberRepository> mockConvert = new Mock<IConvertNumberRepository>();
+                mockConvert.Setup(a => a.ConvertNumberToWord(objectReturn)).Returns(wordNumber);
+                
+                var controllerPorterGroup = StartupController(convertNumberRepository:mockConvert);
+
+
+                IActionResult response = controllerPorterGroup.NumbersToWord(objectReturn);
+
+                return;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+        [Fact]
+        public void ControllerPorterGroup_SumArray_Succsess()
+        {
+            try
+            {
+                int result = 305;
+                SumArrayRequest request = new SumArrayRequest();
+                request.Numbers = new List<int> { 1, 65, 50, 164, 25 };
+
+
+                Mock<ISumNumberRepository> mockSumArray = new Mock<ISumNumberRepository>();
+                mockSumArray.Setup(a => a.SumNumbersArray(request)).Returns(result);
+
+                var controllerPorterGroup = StartupController(sumNumberRepository: mockSumArray);
+
+
+                IActionResult response = controllerPorterGroup.SumArray(request);
+
+                return;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+        [Fact]
+        public void ControllerPorterGroup_OperationsMath_Succsess()
+        {
+            try
+            {
+                int result = 15;
+                var request = new OperationsMathRequest("1+2*5");
+
+
+                Mock<IOperationMathResolveRepository> mockOperationMath = new Mock<IOperationMathResolveRepository>();
+                mockOperationMath.Setup(a => a.ResolveMathOperationStringArray(request)).Returns(result);
+
+                var controllerPorterGroup = StartupController(operationMathResolveRepository: mockOperationMath);
+
+
+                IActionResult response = controllerPorterGroup.OperationsMath(request);
+
+                return;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+        [Fact]
+        public void ControllerPorterGroup_ListUnic_Succsess()
+        {
+            try
+            {
+                List<ObjectUserRequest> request = new List<ObjectUserRequest>();
+                var object1 = new ObjectUserRequest("Paulo", 15);
+                request.Add(object1);
+                var object2 = new ObjectUserRequest("Paulo 2", 26);
+                request.Add(object2);
+                var object3 = new ObjectUserRequest("Paulo", 15);
+                request.Add(object3);
+
+                List<ObjectUserRequest> responseMock = new List<ObjectUserRequest>();
+                responseMock.Add(object1);
+                responseMock.Add(object2);
+
+
+                Mock<IListObjectUnicRepository> mockListObject = new Mock<IListObjectUnicRepository>();
+                mockListObject.Setup(a => a.RemoveRepeatObjects(request)).Returns(responseMock);
+
+                var controllerPorterGroup = StartupController(listObjectUnicRepository: mockListObject);
+
+
+                IActionResult response = controllerPorterGroup.ListUnic(request);
+
+                return;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+
+    }
+}
--- QUESTIONS.md
+++ QUESTIONS.md
@@ -8,13 +8,21 @@
 >
 > 1 - Como você implementou a função que retorna a representação por extenso do número no desafio 1? Quais foram os principais desafios encontrados?
 >
+> R - Esta implementação foi desenvolvida de forma básica utilizando loops e condicionais if e else. A lógica central foi pegar o número de caracteres e fazer o mapeamento de unidade, dezena, centena e milhar, executando as condicionais com os respetivos nomes do número, e sempre que necessário concatenando as palavras correspondentes. Nesta questão coloquei uma regra de que o número poderá ter no máximo 4 caracteres, pois para aumentar os caracteres teria que ter uma implementação maior e possivelmente sendo mais viável criar uma dll só para esta conversão.
+>
+>
 > 2 - Como você lidou com a performance na implementação do desafio 2, considerando que o array pode ter até 1 milhão de números?
+>
+> R - Nesta implementação utilizei da função "Sum" onde ela efetuar esta soma de inteiro dentro do array sem problemas, porém em caso de uma queda de performance na execução, poderia ser implementado uma paginação na lista de forma assíncrona, para haja uma execução mais eficiente da função.
+>
 >
 > 3 - Como você lidou com os possíveis erros de entrada na implementação do desafio 3, como uma divisão por zero ou uma expressão inválida?
 >
+> R - Nesta questão utilizei o método "DataTable" onde ele executa a operação em uma row sem grandes problemas e em caso de erro a mensagem de retorno foi tratado e a mesma, e auto explicativa, e sempre gerando log de erro para análise. Neste caso o "DataTable" já identifica esta operação falha e o mesmo gera uma exceção e assim sendo tratada para retorno.
+>
 > 4 - Como você implementou a função que remove objetos repetidos na implementação do desafio 4? Quais foram os principais desafios encontrados?
 >
->
+> R - A função para excluir objetos repetidos foi implementado utilizando o "DistinctBy" da biblioteca "Linq", com isso a mesma já verifica todos os atributos inserido e assim determina o objeto que esta sendo repetido ou não, com isso ela retorna uma lista apenas com os objetos únicos. Caso e faça esta resolução com loops e comparações a mesma fica complexa para execução, tornando um código menos divergente e com maior possibilidade de falha.
 >
 > </p>
 >
